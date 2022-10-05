866446c39ca78b0cfc97a3926c01372a612d2786
namanraj336 <105062917+namanraj336@users.noreply.github.com>
2022-10-05 14:17:20 +0530

    Add files via upload
    this was the first 4 programs i tried i tried in parallel to learning c with "code with harry" which i wasn't supposed to

---
+++ 2character.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int a = 4;
+float b = 23.5; //this program is just to test character types in c
+char c = 's';
+printf("the value of a is %d \n", a);
+printf("the value of b is %F \n", b);
+printf("the value of c is %C \n", c);
+return 0;
+}
---
+++ 3input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//input two variables and print their sum
+int main(){
+int a,b;
+//now input of first variable
+printf("enter the value of a\n");
+scanf("%d", &a);
+//now input of second variable
+printf("enter the value of b\n");
+scanf("%d", &b);
+//now print the result
+printf("the sum of a and b is %d\n", a + b);
+//now return the value
+return 0;
+}
